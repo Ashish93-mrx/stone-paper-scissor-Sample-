5e234d71504088e7df6ba0f5fec881ded0ddad2f
Ashish N M <76056451+Ashish93-mrx@users.noreply.github.com>
2023-05-10 22:16:29 +0530

    not worked in CSS

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+function sing(lol) {
+    console.log(lol);
+}
+var name="kiki";
+sing("lalalala");
+sing(name);
+
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+multiply(6,2);
+
+var saybye = function() //anonnymous function
+{
+    console.log();
+}
+
+saybye();
+
+//or
+
+var saybye = function fi() //anonnymous function
+{
+    console.log("byee");
+}
+
+saybye();
+
+
+
+// var name=prompt("Enter your name to start");
+
+// if(name=="nayan" || name=="Nayan"){
+//     alert("Ronaldo >>>> Messi,HAHA")
+// }
+// else{
+//     alert("hi "+ name);
+// }
+
+
+
